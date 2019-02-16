9cdd3f4d818e04a01f1149521c03a732b743c050
Pavel.Manakov <james.swift@yandex.ru>
2019-02-17 00:49:20 +0600

    lab 3 task 4 not full

---
+++ s2/labs/lab3/tasks/task4.sql
@@ -0,0 +1,34 @@
+
+create or replace package body p_l3_t4_abstr is
+  chert_count number(10) := 5;
+  procedure getMinChert(pchert in Chert) is
+    naim number(5) := 1;
+    onech varchar2(20);
+  begin
+    onech := pchert(1);
+    
+    for i in 2..5 loop
+      if (length(pchert(i)) < length(onech)) then
+        onech := pchert(i);
+        naim := i;
+      end if;
+    end loop;
+    dbms_output.put_line(onech || ' ' || naim);
+    --return onech;
+  end;
+end;
+
+create or replace package p_l3_t4_abstr is
+  chert_count number(10) := 5;
+  type Chert is varray(5) of varchar2(20);
+  
+  procedure getMinChert(pchert in Chert);
+end p_l3_t4_abstr;
+
+
+declare
+  type Chert is varray(5) of varchar2(20);
+  cherti Chert := Chert('One', 'Two', 'Three', 'Four', 'Five');  
+begin 
+  p_l3_t4_abstr.getMinChert(cherti);
+end;
