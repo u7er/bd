8ec9977cbdaea4acdda814dea6512dc7b430383c
Pavel.Manakov <james.swift@yandex.ru>
2019-02-19 07:48:13 +0600

    done lab3 task 4

--- s2/labs/lab3/tasks/task4.sql
+++ s2/labs/lab3/tasks/task4.sql
@@ -1,34 +1,54 @@
--- not done
+-- backend body
 create or replace package body p_l3_t4_abstr is
-  chert_count number(10) := 5;
-  procedure getMinChert(pchert in Chert) is
-    naim number(5) := 1;
-    onech varchar2(20);
+  procedure getMinChert(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2) is
   begin
-    onech := pchert(1);
-    
-    for i in 2..5 loop
-      if (length(pchert(i)) < length(onech)) then
-        onech := pchert(i);
-        naim := i;
-      end if;
-    end loop;
-    dbms_output.put_line(onech || ' ' || naim);
-    --return onech;
+    if length(rab1) < length(rab2) and length(rab1) < length(rab3) then
+      dbms_output.put_line(rab1);
+    elsif length(rab2) < length(rab3) then
+      dbms_output.put_line(rab2);
+    else
+      dbms_output.put_line(rab3);
+    end if;
+  end;
+  
+  procedure getMaxChert(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2) is
+  begin
+    if length(rab1) > length(rab2) and length(rab1) > length(rab3) then
+      dbms_output.put_line(rab1);
+    elsif length(rab2) > length(rab3) then
+      dbms_output.put_line(rab2);
+    else
+      dbms_output.put_line(rab3);
+    end if;
+  end;
+end;
+-- backend spec
+create or replace package p_l3_t4_abstr is
+  procedure getMinChert(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2);
+  procedure getMaxChert(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2);
+end p_l3_t4_abstr;
+
+-- frontend body
+create or replace package body p_l3_t4 is
+  procedure getMinRab(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2) is
+  begin
+    p_l3_t4_abstr.getMinChert(rab1, rab2, rab3);
+  end;
+  
+  procedure getMaxRab(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2) is
+  begin
+    p_l3_t4_abstr.getMaxChert(rab1, rab2, rab3);
   end;
 end;
 
-create or replace package p_l3_t4_abstr is
-  chert_count number(10) := 5;
-  type Chert is varray(5) of varchar2(20);
-  
-  procedure getMinChert(pchert in Chert);
-end p_l3_t4_abstr;
+-- frontend spec
+create or replace package p_l3_t4 is
+  procedure getMinRab(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2);
+  procedure getMaxRab(rab1 in varchar2, rab2 in varchar2, rab3 in varchar2);
+end p_l3_t4;
 
-
-declare
-  type Chert is varray(5) of varchar2(20);
-  cherti Chert := Chert('One', 'Two', 'Three', 'Four', 'Five');  
+-- call getMax and getMin
 begin 
-  p_l3_t4_abstr.getMinChert(cherti);
+  p_l3_t4.getMinRab('Short', 'Middle', 'Long');
+  p_l3_t4.getMaxRab('Short', 'Middle', 'Long');
 end;
